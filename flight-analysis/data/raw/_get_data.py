dc06bad80df88b0c7ebf9307b5a45d062dc52a34
mt <mt@mt>
2025-04-11 19:08:44 +0800

    add a get data script

--- flight-analysis/data/raw/_get_data.py
+++ flight-analysis/data/raw/_get_data.py
@@ -4,5 +4,12 @@ import subprocess
 # Download latest version
 path = kagglehub.dataset_download("shubhambathwal/flight-price-prediction")
 
-subprocess.run(f'mv {path}/*.csv $(pwd)', shell=True)
+err = subprocess.run(f'mv {path}/*.csv $(pwd)', shell=True, stderr=subprocess.PIPE, text=True)
+
+if err:
+    with open('error.log', 'w') as f:
+        f.write(err)
+        print('ERROR with move the files\nCheck error.log')
+
+
 
